bf4df99a398d028e5cdb8cca98e853c0b1f592e4
YanaSuprun <yana.ysuprun@gmail.com>
2019-09-16 03:29:50 +0300

    Added UI for tas2

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ import {getNumericalSequence} from './src/task6.js';
 import {getFibonacci} from './src/task7.js';
 
 import {chessboardError} from './src/errors.js';
+import {envelopesError} from './src/errors.js';
+
 import {validateChessboard} from './src/validation.js';
+import {validateEnvelopes} from './src/validation.js';
+
 
 const get = function getElement(id) {
   return document.getElementById(id);
@@ -25,11 +29,34 @@ function chessboard () {
   const resultContainer = get('result-chessboard');
 
   if(validateChessboard(height,width,symbol)) {
-    let success = showChessboard(height, width, symbol)
+    let success = showChessboard(height, width, symbol);
     resultContainer.value = `${success}`;
   } else {
     let error = chessboardError.reason;
     resultContainer.value = `${error}`;
+  };
+};
+
+// Task 2
+const btnCheckEnvelopes = get('btn-check-envelopes');
+btnCheckEnvelopes.addEventListener('click', envelopes);
+
+function envelopes () {
+  const a = get('env1-width').value;
+  const b = get('env1-height').value;
+  const c = get('env2-width').value;
+  const d = get('env1-height').value;
+  const resultContainer = get('result-envelope');
+
+  const valid = validateEnvelopes({a, b}, {c, d})
+
+  if(valid) {
+    let success = checkEnvelopes({a, b}, {c, d});
+    resultContainer.innerHTML = `Result: ${success}`;
+  } else {
+    let error = envelopesError.reason;
+    resultContainer.style.color="red";
+    resultContainer.innerHTML = `${error}`;
   }
 }
 
